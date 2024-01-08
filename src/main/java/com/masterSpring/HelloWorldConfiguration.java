1f39d51474b46709855e8e7c3df8b9177c483d95
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-01-08 13:28:33 -0300

    new JavaSpringBeans created

--- src/main/java/com/masterSpring/HelloWorldConfiguration.java
+++ src/main/java/com/masterSpring/HelloWorldConfiguration.java
@@ -2,12 +2,40 @@ package com.masterSpring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age){};
+record Address (String firstLine, String city){};
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name(){
         return "Matias";
+    }
+
+    @Bean
+    public int age(){
+        return 22;
+    }
+
+    @Bean
+    public String firstLine(){
+        return "";
+    }
+
+    @Bean
+    public String city(){
+        return "Punta Alta";
+    }
+
+    @Bean
+    public Person person(){
+        return new Person(name(),age());
+    }
+
+    @Bean
+    public Address address(){
+        return new Address(firstLine(),city());
     }
 
 }
